b03367d3630d7b4c41d5472523760e58bd0c310d
leeyoonseo <okayoon.lee@gmail.com>
2021-03-29 15:14:00 +0900

    함수들 위치 정리

--- src/js/index.js
+++ src/js/index.js
@@ -24,17 +24,113 @@ const toastPopup = document.getElementById('toast-popup');
 
 listAddBtn.addEventListener('click', () => showLightBox('add'));
 listDeleteBtn.addEventListener('click', () => {
-    toggleDeleteMode();
+    changeMode();
 });
 
 listDeleteCloseBtn.addEventListener('click', () => {
-    toggleDeleteMode();
+    changeMode();
     setList(todoData);
 });
 
-listDeleteSaveBtn.addEventListener('click', handlerDeleteSave);
+listDeleteSaveBtn.addEventListener('click', onClickListSave);
 
-function handlerDeleteSave() {
+Array.from(lightBoxCloseBtn).map((v) => {
+    v.addEventListener('click', hideLightBox);
+});
+
+lightBoxSaveBtn.addEventListener('click', onClickLightBoxSave);
+lightBoxDeleteBtn.addEventListener('click', onClickLightBoxDelete);
+lightBoxInput.addEventListener('keypress', ({ key }) => {
+    if (key === 'Enter') {
+        onClickLightBoxSave();
+    }
+})
+
+// [D] init
+setList(todoData);
+
+function onClickItemWrap(e) {
+    let target = e.target;
+
+    if (target.nodeName !== 'DIV') {
+        target = target.parentNode;
+    }
+
+    target.parentNode.removeChild(target);
+}
+
+function onClickItemCopy(e) {
+    let parent = e.target.parentNode;
+
+    if (parent.nodeName !== 'DIV') {
+        parent = parent.parentNode;
+    }
+
+    const target = Array.from(parent.childNodes).filter((v) => v.nodeName === 'INPUT')[0];
+    target.select();
+    document.execCommand("copy");
+
+    showToastPopup('복사 완료');
+}
+
+function onClickLightBoxSave() {
+    if (lightBox.classList.contains('add')) {
+        handlerAddItem();
+    }
+
+    if (lightBox.classList.contains('edit')) {
+        handlerEditItem();
+    }
+}
+
+function handlerAddItem() {
+    const val = lightBoxInput.value;
+    const idx = todoData.length - 1;
+    const addId = todoData[idx] ? todoData[idx].id + 1 : 0;
+    
+    if (!val || !val.trim()) {
+        return alert('텍스트를 입력해주세요.');
+    }
+
+    addItem(addId, val);
+
+    lightBoxInput.value = '';
+
+    hideLightBox();
+    saveStorageData();
+    showToastPopup('추가 완료');
+    setEmptyData();
+}
+
+function handlerEditItem() {
+    const id = lightBoxInput.dataset.id;
+    const val = lightBoxInput.value;
+    const target = findTarget(id);
+
+    if (!val || !val.trim()) {
+        return alert('텍스트를 입력해주세요.');
+    }
+
+    target.value = val;
+
+    editItem(id, val);
+
+    hideLightBox();
+    saveStorageData();
+    showToastPopup('수정 완료');
+}
+
+function onClickLightBoxDelete() {
+    const id = lightBoxInput.dataset.id;
+
+    deleteItem(id);
+    hideLightBox();
+    saveStorageData();
+    showToastPopup('삭제 완료');
+    setEmptyData();
+}
+
+function onClickListSave() {
     const targets = document.querySelectorAll('.item-text');
     let temp = [];
 
@@ -49,30 +145,22 @@ function handlerDeleteSave() {
 
     todoData = temp;
     setList(todoData);
-    toggleDeleteMode();
+    changeMode();
     saveStorageData();
 }
 
-function toggleDeleteMode() {
-    const target = document.querySelector('.js-list-btns');
-    target.classList.toggle('active', !target.classList.contains('active'));
-    todoList.classList.toggle('mode-delete', target.classList.contains('active'));
+function onClickItemCheckbox({ target }) {
+    const id = Number(target.id.split('chk-')[1]);
+    const targetData = todoData.find((v) => v.id === id);
+
+    if (!targetData) return;
+
+    const { parentNode } = target;
+    targetData.isChecked = !targetData.isChecked; 
+
+    parentNode.classList.toggle('checked', targetData.isChecked);
+    saveStorageData();
 }
-
-Array.from(lightBoxCloseBtn).map((v) => {
-    v.addEventListener('click', hideLightBox);
-});
-
-lightBoxSaveBtn.addEventListener('click', handlerLightBoxSave);
-lightBoxDeleteBtn.addEventListener('click', handlerLightBoxDelete);
-lightBoxInput.addEventListener('keypress', ({ key }) => {
-    if (key === 'Enter') {
-        handlerLightBoxSave();
-    }
-})
-
-// [D] init
-setList(todoData);
 
 function setList(data) {
     todoList.innerHTML = '';
@@ -80,42 +168,11 @@ function setList(data) {
     setEmptyData();
 
     data.map((v) => {
-        todoList.appendChild(createItem(v));
+        todoList.appendChild(createItemElements(v));
     });
 }
 
-function setEmptyData() {
-    if (todoData < 1) {
-        createEmptyData();
-    } else {
-        removeEmptyData();
-    }
-}
-function createEmptyData() {
-    const icon = document.createElement('img');
-    const text = document.createElement('span');
-    const iconWrap = document.createElement('div');
-
-    icon.src = 'public/img/icon_cloud-pink.png';
-    text.innerText = 'No data';
-
-    iconWrap.classList.add('empty-icon');
-    iconWrap.id = 'js-empty-icon';
-
-    iconWrap.appendChild(icon);
-    iconWrap.appendChild(text);
-    todoList.appendChild(iconWrap);
-}
-
-function removeEmptyData(){
-    const target = document.getElementById('js-empty-icon');
-
-    if (!target) return;
-
-    target.parentNode.removeChild(target);
-}
-
-function createItem(data) {
+function createItemElements(data) {
     const { id, isChecked, text } = data;
 
     const wrap = document.createElement('div');
@@ -158,105 +215,13 @@ function createItem(data) {
 
     wrap.addEventListener('click', (e) => {
         if (!todoList.classList.contains('mode-delete')) return;
-        handlerDelete(e);
+        onClickItemWrap(e);
     });
 
-    checkbox.addEventListener('change', handlerChangeChk);
-    copyBtn.addEventListener('click', handlerCopy);
+    checkbox.addEventListener('change', onClickItemCheckbox);
+    copyBtn.addEventListener('click', onClickItemCopy);
 
     return wrap;
-}
-
-function handlerDelete(e) {
-    let target = e.target;
-
-    if (target.nodeName !== 'DIV') {
-        target = target.parentNode;
-    }
-
-    // const input = Array.from(target.childNodes).filter((v) => v.nodeName === 'INPUT')[0];
-    // const id = input.dataset.id;
-
-    target.parentNode.removeChild(target);
-}
-
-function handlerCopy(e) {
-    let parent = e.target.parentNode;
-
-    if (parent.nodeName !== 'DIV') {
-        parent = parent.parentNode;
-    }
-
-    const target = Array.from(parent.childNodes).filter((v) => v.nodeName === 'INPUT')[0];
-    target.select();
-    document.execCommand("copy");
-
-    showToastPopup('복사 완료');
-}
-
-function handlerLightBoxSave() {
-    if (lightBox.classList.contains('add')) {
-        handlerAdd();
-    }
-
-    if (lightBox.classList.contains('edit')) {
-        handlerLightBoxEdit();
-    }
-}
-
-function saveStorageData() {
-    localStorage.setItem(TODO_DATA, JSON.stringify(todoData));
-}
-
-function loadStorageData() {
-    return JSON.parse(localStorage.getItem(TODO_DATA) || '[]');
-}
-
-function handlerAdd() {
-    const val = lightBoxInput.value;
-    const idx = todoData.length - 1;
-    const addId = todoData[idx] ? todoData[idx].id + 1 : 0;
-    
-    if (!val || !val.trim()) {
-        return alert('텍스트를 입력해주세요.');
-    }
-
-    addItem(addId, val);
-
-    lightBoxInput.value = '';
-
-    hideLightBox();
-    saveStorageData();
-    showToastPopup('추가 완료');
-    setEmptyData();
-}
-
-function handlerLightBoxEdit() {
-    const id = lightBoxInput.dataset.id;
-    const val = lightBoxInput.value;
-    const target = getTargetElement(id);
-
-    if (!val || !val.trim()) {
-        return alert('텍스트를 입력해주세요.');
-    }
-
-    target.value = val;
-
-    editItem(id, val);
-
-    hideLightBox();
-    saveStorageData();
-    showToastPopup('수정 완료');
-}
-
-function handlerLightBoxDelete() {
-    const id = lightBoxInput.dataset.id;
-
-    deleteItem(id);
-    hideLightBox();
-    saveStorageData();
-    showToastPopup('삭제 완료');
-    setEmptyData();
 }
 
 function addItem(id, val) {
@@ -269,7 +234,7 @@ function addItem(id, val) {
     };
 
     todoData.push(data);
-    todoList.appendChild(createItem(data));
+    todoList.appendChild(createItemElements(data));
 }
 
 function editItem(id, val) {
@@ -281,7 +246,7 @@ function editItem(id, val) {
 }
 
 function deleteItem(id) {
-    const target = getTargetElement(id).parentNode;
+    const target = findTarget(id).parentNode;
     target.parentNode.removeChild(target);
     
     todoData.map((v, i) => {
@@ -291,26 +256,70 @@ function deleteItem(id) {
     });
 }
 
-function getTargetElement(id) {
+function findTarget(id) {
     const targets = document.querySelectorAll('.item-text');
 
     return Array.from(targets)
                 .filter((v) => v.dataset.id === id)[0];
 }
 
-function handlerChangeChk({ target }) {
-    const id = Number(target.id.split('chk-')[1]);
-    const targetData = todoData.find((v) => v.id === id);
-
-    if (!targetData) return;
-
-    const { parentNode } = target;
-    targetData.isChecked = !targetData.isChecked; 
-
-    parentNode.classList.toggle('checked', targetData.isChecked);
-    saveStorageData();
+function changeMode() {
+    const target = document.querySelector('.js-list-btns');
+    target.classList.toggle('active', !target.classList.contains('active'));
+    todoList.classList.toggle('mode-delete', target.classList.contains('active'));
 }
 
+/**
+ * Empty Data
+ */
+function setEmptyData() {
+    if (todoData < 1) {
+        createEmptyData();
+    } else {
+        removeEmptyData();
+    }
+}
+
+function createEmptyData() {
+    const icon = document.createElement('img');
+    const text = document.createElement('span');
+    const iconWrap = document.createElement('div');
+
+    icon.src = 'public/img/icon_cloud-pink.png';
+    text.innerText = 'No data';
+
+    iconWrap.classList.add('empty-icon');
+    iconWrap.id = 'js-empty-icon';
+
+    iconWrap.appendChild(icon);
+    iconWrap.appendChild(text);
+    todoList.appendChild(iconWrap);
+}
+
+function removeEmptyData(){
+    const target = document.getElementById('js-empty-icon');
+
+    if (!target) return;
+
+    target.parentNode.removeChild(target);
+}
+
+/**
+ * Storage
+ * @returns todoData
+ */
+function loadStorageData() {
+    return JSON.parse(localStorage.getItem(TODO_DATA) || '[]');
+}
+
+function saveStorageData() {
+    localStorage.setItem(TODO_DATA, JSON.stringify(todoData));
+}
+
+/**
+ * Toast Popup
+ * @param {string} message - 노출할 메세지
+ */
 function showToastPopup(message) {
     toastPopup.innerText = message;
     toastPopup.classList.add('active');
@@ -326,7 +335,7 @@ function closeToastPopup() {
 }
 
 /**
- * 팝업 show
+ * Light Box
  * @param {string} className - add / edit 
  */
 function showLightBox(className, target) {
@@ -345,7 +354,6 @@ function showLightBox(className, target) {
     lightBoxInput.focus();
 }
 
-/** 팝업 hide */
 function hideLightBox() {
     if (lightBox.classList.contains('add')) {
         lightBox.classList.remove('add');
@@ -359,7 +367,3 @@ function hideLightBox() {
     lightBoxInput.value = '';
     lightBox.classList.remove('opened');
 }
-
-
-// TODO: 
-// -  Empty Data
